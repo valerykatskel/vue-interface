6f47837c6bb47b8eed8a8208812c49a434080283
Valery Katskel <ne.vse.srazu@gmail.com>
2020-01-28 21:11:49 +0300

    Importing Font Awesome icons completed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <div id="main-app">
+  <div id="main-app" class="container">
     <h4>{{ title }}</h4>
+    <font-awesome-icon icon="plus" class="mr-2"/> Add appointment
   </div>
 </template>
 
 <script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 export default {
   name: 'MAinApp',
   data() {
     return {
       title: 'Appointment list'
     }
+  },
+  components: {
+    FontAwesomeIcon
   }
 }
 </script>
